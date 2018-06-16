b810f8aa9092d5b6f8e240264d1e51b33d8446e7
icarodavi <icarodavi@gmail.com>
2018-06-16 18:43:45 -0300

    m2

--- m1/src/index.js
+++ m1/src/index.js
@@ -1,27 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
-import PropTypes from 'prop-types';
 import Button from './Button';
 import './style.scss';
 
 class App extends Component {
   state = {
     counter: 0,
-  }
+  };
 
   handleClick = () => {
-    this.setState(state => ({ counter: this.state.counter + 1 }));
-    this.setState(state => ({ counter: this.state.counter + 1 }));
-  }
-  render() {
-    return(
-    <Fragment>
-      <h1 className="title">Hello</h1>
-      <Button onClick={this.handleClick}>Somar</Button>
-      <h2>{this.state.counter}</h2>
-    </Fragment>
-    );
+    this.setState({ counter: this.state.counter + 1 });
   };
+  render() {
+    return (
+      <Fragment>
+        <h1 className="title">Hello</h1>
+        <Button onClick={this.handleClick}>Somar</Button>
+        <h2>{this.state.counter}</h2>
+      </Fragment>
+    );
+  }
 }
 
-render(<App initialCounter={3} />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
