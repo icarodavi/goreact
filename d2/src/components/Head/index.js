55021669cced3603dcc277c31c1ffc67cd805bd5
icarodavi <icarodavi@gmail.com>
2018-07-03 23:18:43 -0300

    d2

---
+++ d2/src/components/Head/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Head } from './style';
+
+const Head = props => {
+  return (
+    <div>
+      <Head>TExt</Head>
+    </div>
+  );
+};
+
+Head.propTypes = {};
+
+export default Head;
